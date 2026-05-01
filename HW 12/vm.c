496aab0514d4bb0c6a7a316111927b85a955be86
mendojes05 <mendozajessie05@gmail.com>
2026-04-30 23:52:14 -0700

    ch 25 q 1 done

--- HW 12/vm.c
+++ HW 12/vm.c
@@ -107,6 +107,13 @@ static bool callValue(Value callee, int argCount) {
     switch (OBJ_TYPE(callee)) {
       case OBJ_CLOSURE:
         return call(AS_CLOSURE(callee), argCount);
+
+      case OBJ_FUNCTION: {
+        ObjFunction* function = AS_FUNCTION(callee);
+        ObjClosure* temp = newClosure(function);
+        return call(temp, argCount);
+      }
+
       case OBJ_NATIVE: {
         NativeFn native = AS_NATIVE(callee);
         Value result = native(argCount, vm.stackTop - argCount);
@@ -328,8 +335,15 @@ static InterpretResult run() {
       }
       case OP_CLOSURE: {
         ObjFunction* function = AS_FUNCTION(READ_CONSTANT());
+
+        if (function->upvalueCount == 0) {
+          push(OBJ_VAL(function));
+          break;
+        }
+
         ObjClosure* closure = newClosure(function);
         push(OBJ_VAL(closure));
+
         for (int i = 0; i < closure->upvalueCount; i++) {
           uint8_t isLocal = READ_BYTE();
           uint8_t index = READ_BYTE();
@@ -373,10 +387,13 @@ InterpretResult interpret(const char* source) {
   ObjFunction* function = compile(source);
   if (function == NULL) return INTERPRET_COMPILE_ERROR;
 
-  push(OBJ_VAL(function));
-  ObjClosure* closure = newClosure(function);
-  pop();
-  push(OBJ_VAL(closure));
-  call(closure, 0);
+  if (function->upvalueCount > 0) {
+	  ObjClosure* closure = newClosure(function);
+	  pop();
+	  push(OBJ_VAL(closure));
+	  call(closure, 0);
+	} else {
+	  call(newClosure(function), 0);
+	}
   return run();
 }
