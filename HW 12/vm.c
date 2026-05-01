8182cffb374408e5a15434c8f671dd50c69cb79f
mendojes05 <mendozajessie05@gmail.com>
2026-04-30 23:18:38 -0700

    ch 24 q  4 done

--- HW 12/vm.c
+++ HW 12/vm.c
@@ -68,6 +68,33 @@ static void runtimeError(const char* format, ...) {
   resetStack();
 }
 
+static bool typeNative(int argCount, Value* args, Value* result) {
+  if (argCount != 1) {
+    runtimeError("type() expects 1 argument.");
+    return false;
+  }
+
+  Value value = args[0];
+
+  if (IS_NUMBER(value)) {
+    *result = OBJ_VAL(copyString("number", 6));
+  } else if (IS_BOOL(value)) {
+    *result = OBJ_VAL(copyString("bool", 4));
+  } else if (IS_NIL(value)) {
+    *result = OBJ_VAL(copyString("nil", 3));
+  } else if (IS_STRING(value)) {
+    *result = OBJ_VAL(copyString("string", 6));
+  } else if (IS_FUNCTION(value)) {
+    *result = OBJ_VAL(copyString("function", 8));
+  } else if (IS_NATIVE(value)) {
+    *result = OBJ_VAL(copyString("native", 6));
+  } else {
+    *result = OBJ_VAL(copyString("unknown", 7));
+  }
+
+  return true;
+}
+
 static void defineNative(const char* name, NativeFn function) {
   push(OBJ_VAL(copyString(name, (int)strlen(name))));
   push(OBJ_VAL(newNative(function)));
@@ -82,7 +109,8 @@ void initVM() {
   initTable(&vm.globals);
   initTable(&vm.strings);
   defineNative("clock", clockNative);
-  defineNative("sqrt", sqrtNative)
+  defineNative("sqrt", sqrtNative);
+  defineNative("type", typeNative);
 }
 
 void freeVM() {
