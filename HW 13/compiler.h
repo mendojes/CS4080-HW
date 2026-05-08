ede2efbdc06ff325deeb82221238259aadf8bcf4
mendojes05 <mendozajessie05@gmail.com>
2026-05-07 20:06:34 -0700

    ch 26 done

---
+++ HW 13/compiler.h
@@ -0,0 +1,9 @@
+#ifndef clox_compiler_h
+#define clox_compiler_h
+
+#include "object.h"
+
+ObjFunction* compile(const char* source);
+void markCompilerRoots();
+
+#endif
---
+++ HW 13/object.h
@@ -0,0 +1,90 @@
+#ifndef clox_object_h
+#define clox_object_h
+
+#include "common.h"
+#include "chunk.h"
+#include "value.h"
+
+#define OBJ_TYPE(value)        (AS_OBJ(value)->type)
+
+#define IS_CLOSURE(value)      isObjType(value, OBJ_CLOSURE)
+#define IS_FUNCTION(value)     isObjType(value, OBJ_FUNCTION)
+#define IS_NATIVE(value)       isObjType(value, OBJ_NATIVE)
+#define IS_STRING(value)       isObjType(value, OBJ_STRING)
+
+#define AS_CLOSURE(value)      ((ObjClosure*)AS_OBJ(value))
+#define AS_FUNCTION(value)     ((ObjFunction*)AS_OBJ(value))
+#define AS_NATIVE(value)       (((ObjNative*)AS_OBJ(value))->function)
+#define AS_STRING(value)       ((ObjString*)AS_OBJ(value))
+#define AS_CSTRING(value)      (((ObjString*)AS_OBJ(value))->chars)
+
+typedef struct Obj Obj;
+typedef struct ObjClosure ObjClosure;
+typedef struct ObjFunction ObjFunction;
+typedef struct ObjNative ObjNative;
+typedef struct ObjString ObjString;
+typedef struct ObjUpvalue ObjUpvalue;
+
+typedef Value (*NativeFn)(int argCount, Value* args);
+
+typedef enum {
+  OBJ_CLOSURE,
+  OBJ_FUNCTION,
+  OBJ_NATIVE,
+  OBJ_STRING,
+  OBJ_UPVALUE,
+} ObjType;
+
+struct Obj {
+  ObjType type;
+  bool isMarked;
+  struct Obj* next;
+};
+
+struct ObjFunction {
+  Obj obj;
+  int arity;
+  int upvalueCount;
+  Chunk chunk;
+  ObjString* name;
+};
+
+struct ObjClosure {
+  Obj obj;
+  ObjFunction* function;
+  ObjUpvalue** upvalues;
+  int upvalueCount;
+};
+
+struct ObjNative {
+  Obj obj;
+  NativeFn function;
+};
+
+struct ObjString {
+  Obj obj;
+  int length;
+  char* chars;
+  uint32_t hash;
+};
+
+struct ObjUpvalue {
+  Obj obj;
+  Value* location;
+  Value closed;
+  ObjUpvalue* next;
+};
+
+ObjClosure* newClosure(ObjFunction* function);
+ObjFunction* newFunction();
+ObjNative* newNative(NativeFn function);
+ObjUpvalue* newUpvalue(Value* slot);
+ObjString* takeString(char* chars, int length);
+ObjString* copyString(const char* chars, int length);
+void printObject(Value value);
+
+static inline bool isObjType(Value value, ObjType type) {
+  return IS_OBJ(value) && AS_OBJ(value)->type == type;
+}
+
+#endif
