90d48e0cbd57d522508d912e9f2634a076683d43
mendojes05 <mendozajessie05@gmail.com>
2026-05-12 19:16:38 -0700

    ch 29 q 1 done

--- HW 14/compiler.c
+++ HW 14/compiler.c
@@ -322,17 +322,38 @@ static void call(bool canAssign) {
 }
 
 static void dot(bool canAssign) {
+  bool wasThis = thisAccess;
+  thisAccess = false;
+
   consume(TOKEN_IDENTIFIER, "Expect property name after '.'.");
-  uint8_t name = identifierConstant(&parser.previous);
+  Token field = parser.previous;
+
+  bool mangle = wasThis && currentClass != NULL;
+  uint8_t plainName = 0;
+  bool plainBuilt = false;
 
   if (canAssign && match(TOKEN_EQUAL)) {
     expression();
+
+    uint8_t name = mangle
+        ? fieldConstant(&currentClass->name, &field)
+        : makeConstant(OBJ_VAL(copyString(field.start, field.length)));
+
     emitBytes(OP_SET_PROPERTY, name);
   } else if (match(TOKEN_LEFT_PAREN)) {
+    if (!plainBuilt) {
+      plainName = makeConstant(OBJ_VAL(copyString(field.start, field.length)));
+      plainBuilt = true;
+    }
+
     uint8_t argCount = argumentList();
-    emitBytes(OP_INVOKE, name);
+    emitBytes(OP_INVOKE, plainName);
     emitByte(argCount);
   } else {
+    uint8_t name = mangle
+        ? fieldConstant(&currentClass->name, &field)
+        : makeConstant(OBJ_VAL(copyString(field.start, field.length)));
+
     emitBytes(OP_GET_PROPERTY, name);
   }
 }
@@ -534,6 +555,18 @@ static void parsePrecedence(Precedence precedence) {
 
 static uint8_t identifierConstant(Token* name) {
   return makeConstant(OBJ_VAL(copyString(name->start, name->length)));
+}
+
+static uint8_t fieldConstant(Token* className, Token* field) {
+  int len = className->length + 1 + field->length;
+  char* buf = ALLOCATE(char, len + 1);
+
+  memcpy(buf, className->start, className->length);
+  buf[className->length] = '$';
+  memcpy(buf + className->length + 1, field->start, field->length);
+  buf[len] = '\0';
+
+  return makeConstant(OBJ_VAL(takeString(buf, len)));
 }
 
 static bool identifiersEqual(Token* a, Token* b) {
